6755b669a72190dd52e1c2e94a93657d36d0f261
David Dollar <david@dollar.io>
2023-05-07 11:56:08 -0400

    updates

--- symlinks.rb
+++ symlinks.rb
@@ -7,9 +7,11 @@ Pathname.glob('*').select(&:directory?).sort.each do |image|
     (0..v.segments.length - 2).map { |p| v.segments[0..p].join('.') }.each do |parent|
       ax[parent] = [ax[parent], v].compact.max
     end
+    ax['latest'] = versions.last
   end
   Dir.chdir(image) do
-    links.each do |link, version|
+    links.sort.each do |link, version|
+      version = links[link]
       puts "  #{version} -> #{link}"
       File.unlink(link) if File.symlink?(link)
       File.symlink(version.to_s, link)
